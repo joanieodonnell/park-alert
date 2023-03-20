4cc3ca6c76ed6ad6edda8f979ec616b7f45199d8
joanieodonnell <125310683+joanieodonnell@users.noreply.github.com>
2023-03-19 22:48:21 -0500

    initial commit

---
+++ ParkAlert/ParkAlert/ParkAlertApp.swift
@@ -0,0 +1,17 @@
+//
+//  ParkAlertApp.swift
+//  ParkAlert
+//
+//  Created by Joanie O'Donnell on 3/7/23.
+//
+
+import SwiftUI
+
+@main
+struct ParkAlertApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
